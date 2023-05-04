17788175e40df9bf4491ec26e9868db8592c4cc6
Deepanshu <deepanshumehra2001@gmail.com>
2023-05-04 10:09:11 +0530

    added post route to send string to evaluate

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,8 +1,18 @@
-import miningLogic
+from miningLogic import get_words_using_sentence  
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return '<h1> Hello, World! </h1>'
+
+@app.route('/getWordsFromScenario', methods=['POST'])
+def getWordsFromScenario():
+    if request.method == 'POST':
+        json = request.get_json()
+        request_string = json['string']
+        out = get_words_using_sentence(request_string)
+        print(out)
+        return 'Success Printing',200
--- flask-backend/miningLogic.py
+++ flask-backend/miningLogic.py
@@ -1,41 +1,33 @@
+from pydoc import doc
 from keybert import KeyBERT
 from transformers import DistilBertTokenizer, DistilBertModel, pipeline
 
 
-doc = """
-I am Thirsty
-      """
-kw_model = KeyBERT()
-keywords = kw_model.extract_keywords(doc)
+def get_words_using_sentence(doc):
+    kw_model = KeyBERT()
+    keywords = kw_model.extract_keywords(doc)
+    extracted_words = kw_model.extract_keywords(doc, keyphrase_ngram_range=(1, 1), stop_words=None)
+    doc_arr = doc.replace('\n', '').split(' ')
+    def listToString(s):
+        str1 = ""
+        for ele in s:
+            str1 += ele
+        return str1
+    for i in range(0,len(extracted_words)):
+        for j in range(0,len(doc_arr)):
+            if(extracted_words[i][0].lower() == doc_arr[j].lower()):
+                doc_arr[j] = '[MASK]'
+    masked_statement = listToString(doc_arr)
+    # tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    # model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+    text = "[MASK] me by any text you'd like."
+    # encoded_input = tokenizer(text, return_tensors='pt')
+    # output = model(**encoded_input).logits
+    unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
+    result = unmasker(masked_statement)
+    return result
 
-extracted_words = kw_model.extract_keywords(doc, keyphrase_ngram_range=(1, 1), stop_words=None)
-
-doc_arr = doc.replace('\n', '').split(' ')
-
-def listToString(s):
-    str1 = ""
-    for ele in s:
-        str1 += ele
-    return str1
-
-
-for i in range(0,len(extracted_words)):
-    for j in range(0,len(doc_arr)):
-        if(extracted_words[i][0].lower() == doc_arr[j].lower()):
-            doc_arr[j] = '[MASK]'
-
-masked_statement = listToString(doc_arr)
-
-
-# tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-# model = DistilBertModel.from_pretrained("distilbert-base-uncased")
-text = "[MASK] me by any text you'd like."
-# encoded_input = tokenizer(text, return_tensors='pt')
-# output = model(**encoded_input).logits
-
-unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
-result = unmasker(masked_statement)
-print(result)
+print(get_words_using_sentence('what is the first'))
 
 # todos: 
 # limit to 3 mask 
