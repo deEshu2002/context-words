115173f08db2d19f3b13756c6864b1d59c8fa874
Deepanshu <deepanshumehra2001@gmail.com>
2023-05-05 19:00:05 +0530

    changed word getting algorithms to provide more readable content

--- flask-backend/miningLogic.py
+++ flask-backend/miningLogic.py
@@ -1,34 +1,63 @@
 from pydoc import doc
 from keybert import KeyBERT
-from transformers import DistilBertTokenizer, DistilBertModel, pipeline
+from functools import reduce
+import re
+import string
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+import torch
 
+model_checkpoint = "distilbert-base-uncased"
 
-def get_words_using_sentence(doc):
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += (ele + " ")
+    return str1
+
+def transform_input_to_mask_words(input):
     kw_model = KeyBERT()
-    keywords = kw_model.extract_keywords(doc)
-    extracted_words = kw_model.extract_keywords(doc, keyphrase_ngram_range=(1, 1), stop_words=None)
-    doc_arr = doc.replace('\n', '').split(' ')
-    def listToString(s):
-        str1 = ""
-        for ele in s:
-            str1 += ele
-        return str1
-    for i in range(0,len(extracted_words)):
-        for j in range(0,len(doc_arr)):
-            if(extracted_words[i][0].lower() == doc_arr[j].lower()):
-                doc_arr[j] = '[MASK]'
-    masked_statement = listToString(doc_arr)
-    # tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-    # model = DistilBertModel.from_pretrained("distilbert-base-uncased")
-    text = "[MASK] me by any text you'd like."
-    # encoded_input = tokenizer(text, return_tensors='pt')
-    # output = model(**encoded_input).logits
-    unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
-    result = unmasker(masked_statement)
-    return result
+    keyword = kw_model.extract_keywords(input)
+    extracted_words = kw_model.extract_keywords(input, keyphrase_ngram_range=(1, 1), stop_words=None)
+    input_arr = input.replace('\n', '').split(' ')
 
-print(get_words_using_sentence('what is the first'))
+    output_arr =[]
+    for i in range(0,3):
+        for j in range(0,len(input_arr)):
+            if(extracted_words[i][0].lower() == input_arr[j].lower()):
+                initial_word = input_arr[j]
+                input_arr[j] = '[MASK]'
+                output_arr.append(listToString(input_arr))
+                input_arr[j] = initial_word
+    return output_arr
 
-# todos: 
-# limit to 3 mask 
-# and limit to 3 mask value
+def format_keywords(keywords_array):
+    special_chars = string.punctuation
+    output = {}
+    for i in keywords_array:
+        for j in i:
+            bools = list(map(lambda char: char in special_chars, j ))
+            if not any(bools):
+                output[j] = j
+    return output
+
+
+def get_words(input):
+    masked_sentences =  transform_input_to_mask_words(input)
+    model = AutoModelForMaskedLM.from_pretrained(model_checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    keyword_array_2D = []
+    for sentence in masked_sentences:
+        inputs = tokenizer(sentence, return_tensors='pt' )
+        token_logits = model(**inputs).logits
+        mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+        mask_token_logits = token_logits[0, mask_token_index, :]
+        top_3_tokens = torch.topk(mask_token_logits, 4, dim=1).indices[0].tolist()
+        keyword_array = []
+        for token in range(1,len(top_3_tokens)):
+            keyword_array.append(tokenizer.decode(top_3_tokens[token]))
+        keyword_array_2D.append(keyword_array)
+    formatted_words = format_keywords(keyword_array_2D)
+    return formatted_words
+
+
+# print(get_words('Paris is the capital of France'))
